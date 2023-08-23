3a6f75d1f0ed3a0eced96ef6b98f4621d40ff652
janepausirator2456 <143038034+janepausirator2456@users.noreply.github.com>
2023-08-23 21:30:37 +0000

    real

--- e7.c
+++ e7.c
@@ -8,21 +8,27 @@ typedef struct {
 } Point;
 
 typedef struct {
+    int real;
+    int imag;
+} Complex;
+
+typedef struct {
     int a;
     int b;
     int c;
+    Complex complex;
 } ComplexValues;
 
 void performComplexCalculation(Point* point, int iterations, ComplexValues* complexValues) {
     for (int i = 0; i < iterations; ++i) {
-        point->x = point->x * complexValues->a - point->y * complexValues->b + complexValues->c;
-        point->y = complexValues->a * point->x * point->y + complexValues->b * point->y + complexValues->c;
+        point->x = point->x * complexValues->a - point->y * complexValues->b + complexValues->c + complexValues->complex.real;
+        point->y = complexValues->a * point->x * point->y + complexValues->b * point->y + complexValues->c + complexValues->complex.imag;
     }
 }
 
 int main() {
     Point complexPoint = {1, 1};
-    ComplexValues complexValues = {2, 3, 4};
+    ComplexValues complexValues = {2, 3, 4, {5, 6}};
     int totalIterations = 0;
 
     while (totalIterations < MAX_ITERATIONS) {
